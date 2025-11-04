04af9778ab8d350292d5336569cc44b7f9d26572
Donald Maness <ddmaness@gmail.com>
2025-11-04 13:47:40 -0500

    Complete Part 2 a

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+const PersonForm = ({ onAddPerson, newName, setNewName, newNumber, setNewNumber }) => (
+  <form>
+    <div>
+      name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+    </div>
+    <div>
+      <button type="submit" onClick={onAddPerson}>add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map(person => 
+      <p key={person.name}>{person.name} {person.number}</p>  
+    )}
+  </div>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleAddPerson = (e) => {
+    e.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+   if (newName.trim() === '') {
+      alert('Name cannot be empty')
+      return
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <PersonForm 
+        onAddPerson={handleAddPerson} 
+        newName={newName} 
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  )
+}
+
+export default App
